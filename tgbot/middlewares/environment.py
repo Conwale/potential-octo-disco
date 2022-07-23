ce0054af54973ff1d5050310582c95406f18514b
Rewale <komaroff.sania2015@gmail.com>
2022-07-23 21:48:13 +0500

    Исправлены ошибки шаблона

--- tgbot/middlewares/environment.py
+++ tgbot/middlewares/environment.py
@@ -4,7 +4,8 @@ from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
 class EnvironmentMiddleware(LifetimeControllerMiddleware):
     skip_patterns = ["error", "update"]
     
-    def __init__(self, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__()
         self.kwargs = kwargs
     
     async def pre_process(self, obj, data, *args):
